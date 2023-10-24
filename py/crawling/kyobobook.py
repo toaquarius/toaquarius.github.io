3353c51d708b0725012210838618dd46093c31ac
toaquarius <148832532+toaquarius@users.noreply.github.com>
2023-10-24 13:11:13 +0900

    Update kyobobook.py

--- py/crawling/kyobobook.py
+++ py/crawling/kyobobook.py
@@ -24,7 +24,7 @@ class kyobo():
 			
 	def getBookList(self):
 		v_page = askUtil.getRandom(1,5)
-		# v_page = 4
+		v_page = 2
 		print("v_page : ",v_page)
 		url = f'https://product.kyobobook.co.kr/api/gw/pub/pdt/best-seller/online?page={v_page}&per=100&period=001&dsplDvsnCode=000&dsplTrgtDvsnCode=001'
 		
@@ -33,11 +33,13 @@ class kyobo():
 			bookData = requests.get(url,headers={'User-Agent': 'Mozilla/5.0'}, timeout=25).json()	
 			cnt = 0
 			for i in bookData['data']['bestSeller']:
-				# print("i ",i)		
 				isbn 				= askUtil.custUtil(i, 'cmdtCode') 
-				if self.ad.df_isOrder(isbn) > 0:
-					continue
 				cnt+=1
+
+				if self.ad.df_isOrder(isbn) > 0:
+					print("continue : ", cnt)
+					continue
+				print("for :  ",cnt, ' isbn:',isbn)	
 				saleCmdtid 			= askUtil.custUtil(i, 'saleCmdtid') #"saleCmdtid": "S000061863239",
 				saleCmdtClstName	= askUtil.custUtil(i, 'saleCmdtClstName') #"saleCmdtClstName": "경제전망",
 				book_nm 			= askUtil.custUtil(i, 'cmdtName') #"cmdtName": "트렌드 코리아 2023",
@@ -51,15 +53,19 @@ class kyobo():
 							
 				try:
 					naver_api_book_result 			= self.nvApi.bookSearch(isbn)
-					# print("naver_api_book_result : ",naver_api_book_result)
+					time.sleep(1)
+					print("naver_api_book_result : ",naver_api_book_result)
 					# description			=  askUtil.custUtil(naver_api_book_result,'description')
 					# if not description:
 					description = desc
 					coverLargeUrl 		= naver_api_book_result['image']
 					coverSmallUrl 		= naver_api_book_result['image']
+					
 				except Exception as e:
 					description = desc			
 					print("################## nv e ##########: ",e)
+					print("isbn : ", isbn)
+				
 					# print(" nv API err  : ",i)
 				
 				link_c 				= ''
@@ -106,10 +112,10 @@ class kyobo():
 						self.ad.df_orderInsert(isbn, 'N')
 						print("cnt : ", str(cnt)," ISBN_NO : ",isbn)
 					
-					if len(reviewList) >= 5:
+					if len(reviewList) >= 3:
 						self.book_list.append( {"BOOK_NM":book_nm,"BOOK_IMG_L_URL":coverLargeUrl,
 											"BOOK_IMG_S_URL":coverSmallUrl,"AUTHOR":author,"ISBN_NO":isbn,"PUB_SR":publisher,"PUB_DT":pubDate,"BOOK_CD":book_cd,
-											"REVIEW_LIST":reviewList,"BOOK_DESC":description,"BOOK_DESC2":description2,"BOOK_DESC3":description3
+											"REVIEW_LIST":reviewList,"BOOK_DESC":description,"BOOK_DESC2":description2,"BOOK_DESC3":description3,"RW_CNT":rwcnt
 											})
 					else:
 						self.ad.df_orderInsert(isbn, 'N')
@@ -126,4 +132,12 @@ class kyobo():
 
 		#https://crontab.guru/
 # if __name__ == '__main__':		
-# 		print("kyobobook")
+# 	print("kyobobook")
+	
+# 	import ask_db
+# 	from API import naverApi
+	
+# 	ad = ask_db.AskDb()
+# 	kbb = kyobo(ad,naverApi.NaverApi())
+# 	bookList = kbb.getBookList()
+
